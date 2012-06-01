d88757610a1f579ec1f205d8a1d4eee492defbfe
Michael Lee <m.jeremy.lee@gmail.com>
2012-05-31 21:10:41 -0700

    Working Update, next update should recycle the cars after running the length of the road

--- Traffic_Animation.py
+++ Traffic_Animation.py
@@ -152,7 +152,7 @@ def Traffic_Animation(space,l,time,lane,density):
     canvas.pack()
     
     for i in range(n):
-        cars[i] = canvas.create_rectangle(w*space[i][0],20,w*space[i][0]+w,20+w,fill="red", tag=i)
+        cars[i] = canvas.create_rectangle(w*space[i][0],20,w*space[i][0]+w,20+w,fill="red")
         
     for i in range(l-1):
         spots[i] = canvas.create_line(w*(i+1),0,w*(i+1),lane*(w+50))
@@ -166,13 +166,18 @@ def Traffic_Animation(space,l,time,lane,density):
             p2 = space[k][t+1]
             v[k].append(int((round((p2 - p1)*w/10))))
     
+    
     for t in range(time - 1):
         for p in range(10):
             for i in range(n):
-                canvas.move(i,v[i][t],0)
+                canvas.move(cars[i],v[i][t],0)
                 canvas.after(20)
                 canvas.update()
 
+    window.mainloop()
+    return v
+    
+v = Traffic_Animation(space,l,time,lane,0.5)
     
 Traffic_Animation(space,l,time,lane,0.5)
 
